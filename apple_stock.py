3939d033de1274eae5a1d99f34dd62480d04cf96
MusharratZ <musharrat.parveen14@spsmail.cuny.edu>
2023-10-30 22:10:19 -0400

    with full complete code

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,35 @@
-if __name__ == "__main__":
-    pass
+"""url = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+"""
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+}
+url = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+page = requests.get(url, headers=headers)
+page.raise_for_status()
+if page.status_code == 200:
+    soup = BeautifulSoup(page.text, "html.parser")
+    table = soup.find("table", {"data-test": "historical-prices"})
+    if table:
+        rows = table.find_all("tr")
+
+        if len(rows) >= 2:
+            print("Date           Close")
+
+            for row in rows[1:]:
+                columns = row.find_all("td")
+
+                if len(columns) >= 5:
+                    date = columns[0].get_text()
+                    close_price = columns[4].get_text()
+                    print(f"{date}  {close_price}")
+                else:
+                    print("Data is not in the expected format.")
+        else:
+            print("No data rows found in the table.")
+    else:
+        print("Table not found on the page.")
+else:
+    print("Failed to retrieve the webpage.")
--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,52 @@
-if __name__ == "__main__":
-    pass
+"""
+       url = "https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/"
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+}
+url = "https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/"
+page = requests.get(url, headers=headers)
+# print(page.raise_for_status)
+
+if page.status_code == 200:
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    table = soup.find("table", {"class": "TableBase-table"})
+
+    rows = table.find_all("tr")
+
+    data = []
+    for row in rows:
+        columns = row.find_all("td")
+
+        # Ensure that there are at least 3 columns in each row
+        if len(columns) >= 3:
+            player_name = columns[0].find("a").get_text(strip=True)
+            position = (
+                columns[0]
+                .find("span", class_="CellPlayerName-position")
+                .get_text(strip=True)
+            )
+            team = (
+                columns[0]
+                .find("span", class_="CellPlayerName-team")
+                .get_text(strip=True)
+            )
+            touchdown = columns[8].get_text(strip=True)
+
+            data.append([player_name, position, team, touchdown])
+
+    df = pd.DataFrame(data, columns=["Player Name", "Position", "Team", "Touchdowns"])
+    df.set_index(df.columns[0], inplace=True, drop=True)
+    # Print the top 20 players
+    top_20_players = df.head(20)
+    print(top_20_players)
+
+
+else:
+    print("Failed to retrieve the webpage.")
